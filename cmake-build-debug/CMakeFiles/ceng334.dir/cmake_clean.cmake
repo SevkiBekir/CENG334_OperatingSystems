4abde5f8d8ebc4b4ee36bd53ff021a4f611f34e0
SevkiBekir <bekirsevki@gmail.com>
2018-03-12 04:24:49 +0300

    the1-file read and output structure were done.

---
+++ cmake-build-debug/CMakeFiles/ceng334.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ceng334.dir/the1.c.o"
+  "ceng334.pdb"
+  "ceng334"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ceng334.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
